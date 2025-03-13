d7758303dbb682f2c3c93b90c51ff0e0d2a13e44
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-03-13 12:42:51 +0530

    Time: 5 ms (81.18%), Space: 43.8 MB (63.64%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+         HashSet<char> charSet = new HashSet<char>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.Length; right++) {
+            while (charSet.Contains(s[right])) {
+                charSet.Remove(s[left]);
+                left++;
+            }
+            charSet.Add(s[right]);
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
