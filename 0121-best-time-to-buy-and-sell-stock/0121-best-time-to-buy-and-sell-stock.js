e6939eb397a9b6711543ea7b0575ffc4d89921ca
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-03-13 10:40:47 +0530

    Time: 7 ms (14.2%), Space: 65.3 MB (15.43%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,9 @@
+var maxProfit = function(prices) {
+    let minPrice= Infinity;
+    let maxProfit= 0;
+    for(let price of prices){
+    minPrice=Math.min(minPrice,price)
+    maxProfit=Math.max(maxProfit,price-minPrice)
+}
+    return maxProfit
+};
