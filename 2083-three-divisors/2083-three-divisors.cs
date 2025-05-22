cc49abdea9ce9888b29cfc53f7175c4eb0648825
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-05-22 09:51:22 +0530

    Time: 1 ms (69.32%), Space: 30.9 MB (77.27%) - LeetHub

---
+++ 2083-three-divisors/2083-three-divisors.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsThree(int n) {
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count==3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
