5427bd12ace634c1c7df11f2850fc249f0e72bec
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-06-03 10:03:27 +0530

    Time: 1 ms (72.73%), Space: 29.6 MB (13.64%) - LeetHub

---
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CountEven(int num) {
+        int count = 0;
+        for(int i=1;i<=num;i++){
+            int sum = 0;
+            int n= i;
+            while(n > 0){
+                sum += n%10;
+                n/=10;
+            }
+            if(sum%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
