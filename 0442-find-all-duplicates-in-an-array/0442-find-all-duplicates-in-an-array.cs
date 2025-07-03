fec371825e2cf6479f65d6f9f4c7710adb565fd9
Muhammed Sanoof <sanoofmohammed.pvt@gmail.com>
2025-07-03 14:45:10 +0530

    Time: 2730 ms (5.15%), Space: 70.9 MB (17.28%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cs
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cs
@@ -1,14 +1,12 @@
 public class Solution {
     public IList<int> FindDuplicates(int[] nums) {
-        HashSet<int> seen = new HashSet<int>();
-        List<int> duplicates = new List<int>();
-
-        foreach (int num in nums) {
-            if (!seen.Add(num)) {
-                duplicates.Add(num);
+        HashSet<int> res = new HashSet<int>();
+        
+        foreach(int i in nums){
+            if(Array.IndexOf(nums,i) != Array.LastIndexOf(nums,i)){
+                res.Add(i);
             }
         }
-
-        return duplicates;
+        return res.ToList();
     }
 }
